d023eb1fa84e830b9b21f0e7eb8e034588635a60
RomainP01 <romainpanii@gmail.com>
2022-02-13 15:54:14 +0100

    wip cas pratique 1 test unitaire

---
+++ CasPratique_1/Solution1/EasyBank/BankServices.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EasyBank
+{
+    public class BankServices
+    {
+        private List<BankAccount> _bankAccounts;
+
+        public BankServices(List<BankAccount> bankAccounts)
+        {
+            _bankAccounts = bankAccounts;
+        }
+
+        public List<BankAccount> BankAccounts
+        {
+            get => _bankAccounts;
+            set => _bankAccounts = value;
+        }
+        
+
+        
+        public BankAccount GetBankAccountById(int id)
+        {
+            foreach (var bankAccount in _bankAccounts)
+            {
+                if (bankAccount.Id == id)
+                {
+                    return bankAccount;
+                }
+            }
+            throw new ArgumentException("Aucun compte avec l'identifiant renseigné a été trouvé");
+        }
+    }
+}
--- CasPratique_1/Solution1/TestEasyBank/UnitTest1.cs
+++ CasPratique_1/Solution1/TestEasyBank/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using EasyBank;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -16,5 +17,49 @@ namespace TestEasyBank
             Assert.AreEqual("Romain", bankAccount.FirstName);
             Assert.AreEqual(10000.99, bankAccount.Balance);
         }
+        
+        [TestMethod]
+        [Description("GIVEN: Etant donné une recherche de compte client, "+
+                     "WHEN : lorsque je recherche le compte du client avec l'identifiant 2, "+
+                     "THEN : alors l'objet BankAccount du client avec l'identifiant 2 est retourné")]
+        public void Test_FindAnAccountById_Exist_ReturnBankAccount()
+        {    
+            //Arrange
+            BankAccount bankAccount = new BankAccount(1, "Romain", "PANI", 10000.99);
+            BankAccount bankAccount2 = new BankAccount(2, "Robert", "HALLIDAY", 10.53);
+            List<BankAccount> list = new List<BankAccount>();
+            list.Add(bankAccount);
+            list.Add(bankAccount2);
+            BankServices bankServices = new BankServices(list);
+            
+            //Act
+            BankAccount bankAccountFindWithId = bankServices.GetBankAccountById(2);
+            
+            //Assert
+            Assert.AreSame(bankAccountFindWithId,bankAccount2);
+
+        }
+        
+        [TestMethod]
+        [Description("GIVEN: Etant donné une recherche de compte client, "+
+                     "WHEN : lorsque je recherche le compte du client avec l'identifiant 0, "+
+                     "THEN : alors aucun client n'est trouvé et ainsi une Exception de type Argument Exception est levée")]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void Test_FindAnAccountById_NotExist_ReturnException()
+        {    
+            //Arrange
+            BankAccount bankAccount = new BankAccount(1, "Romain", "PANI", 10000.99);
+            BankAccount bankAccount2 = new BankAccount(2, "Robert", "HALLIDAY", 10.53);
+            List<BankAccount> list = new List<BankAccount>();
+            list.Add(bankAccount);
+            list.Add(bankAccount2);
+            BankServices bankServices = new BankServices(list);
+            
+            //Act & Assert
+            BankAccount bankAccountFindWithId = bankServices.GetBankAccountById(0);
+
+        }
     }
+    
+    
 }
